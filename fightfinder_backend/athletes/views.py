f20f003187916f5d331448fae5dc58e0a37ba214
castilho <guilherme.castilhot2c18@gmail.com>
2024-08-29 22:56:09 -0300

    .

--- fightfinder_backend/athletes/views.py
+++ fightfinder_backend/athletes/views.py
@@ -5,11 +5,53 @@ from .recommender import recommend_opponents
 from athletes.models import Athlete
 from athletes.serializers import AthleteSerializer
 
+from datetime import date
 
-class AthleteCreateListView(generics.ListCreateAPIView):
+
+def calcular_idade(data_nascimento):
+    today = date.today()
+    return (
+        today.year
+        - data_nascimento.year
+        - ((today.month, today.day) < (data_nascimento.month, data_nascimento.day))
+    )
+
+
+class AthleteCreateListView(generics.ListAPIView):
     # permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Athlete.objects.all()
     serializer_class = AthleteSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        peso_min = self.request.query_params.get("peso_min")
+        peso_max = self.request.query_params.get("peso_max")
+        idade_min = self.request.query_params.get("idade_min")
+        idade_max = self.request.query_params.get("idade_max")
+        modalidade = self.request.query_params.get("modalidade")
+
+        if modalidade:
+            queryset = queryset.filter(modalidade=modalidade)
+
+        if idade_min is not None:
+            queryset = queryset.filter(
+                data_nascimento__lte=date.today().replace(
+                    year=date.today().year - int(idade_min)
+                )
+            )
+        if idade_max is not None:
+            queryset = queryset.filter(
+                data_nascimento__gte=date.today().replace(
+                    year=date.today().year - int(idade_max)
+                )
+            )
+
+        if peso_min is not None:
+            queryset = queryset.filter(peso__gte=peso_min)
+        if peso_max is not None:
+            queryset = queryset.filter(peso__lte=peso_max)
+
+        return queryset
 
 
 class AthleteRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
