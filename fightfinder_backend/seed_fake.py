a58982cb0045247b939e0615ea9dbbac18b86166
castilho <guilherme.castilhot2c18@gmail.com>
2024-08-29 21:25:06 -0300

    .

--- fightfinder_backend/seed_fake.py
+++ fightfinder_backend/seed_fake.py
@@ -108,4 +108,4 @@ def seed_database(num_athletes=10, num_fights=5, num_events=3):
 
 
 if __name__ == "__main__":
-    seed_database()
+    seed_database(num_athletes=1000, num_fights=900, num_events=100)
