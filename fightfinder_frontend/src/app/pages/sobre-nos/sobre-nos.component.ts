1d79bb463371f20c4bffce4c1e6c8faff59eafd0
danilolugli <101900217+danilolugli@users.noreply.github.com>
2024-09-02 12:01:15 -0300

    pagina sobre-nos

--- fightfinder_frontend/src/app/pages/sobre-nos/sobre-nos.component.ts
+++ fightfinder_frontend/src/app/pages/sobre-nos/sobre-nos.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sobre-nos',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './sobre-nos.component.html',
   styleUrl: './sobre-nos.component.css'
 })
 export class SobreNosComponent {
 
+  dropdown: boolean = false;
+
+  mostraDesc(): void { 
+    this.dropdown = !this.dropdown;
+  }
 }
