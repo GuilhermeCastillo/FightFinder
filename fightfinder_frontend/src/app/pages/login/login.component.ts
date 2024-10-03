e7ee84ed3bb1c62845166ad6465693be2b0f257f
danilolugli <101900217+danilolugli@users.noreply.github.com>
2024-10-03 20:26:27 -0300

    llasdjklakdsa

--- fightfinder_frontend/src/app/pages/login/login.component.ts
+++ fightfinder_frontend/src/app/pages/login/login.component.ts
@@ -4,21 +4,51 @@ import { InputSenhaComponent } from '../../components/inputSenha/inputSenha.comp
 import { ButtonComponent } from '../../components/button/button.component'; 
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ ButtonComponent, InputSenhaComponent, InputTextoComponent, CommonModule ],
+  imports: [ ButtonComponent, InputSenhaComponent, InputTextoComponent, CommonModule, HttpClientModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
-export class LoginComponent {
-  constructor(private router: Router) { }
-
+export class LoginComponent { 
   userPassword: string = '';
+  respostaApi: any;
+
+  constructor(private router: Router, private http: HttpClient) { }
+
 
   entrar(): void {
     this.router.navigate(['/home']);
+  }
+
+  enviarDados(): void {
+    var email: string = "abcdeImpacta@gmail.com";
+    var username: string = "Célio Takai";
+    var password1: string = "impactaCelioTakai123";
+    var password2: string = "impactaCelioTakai123";
+ 
+    const dados = {
+      email: email,
+      username: username,
+      password1: password1,
+      password2: password2
+    };
+
+    const url: string = "http://127.0.0.1:8000/api/register"; 
+
+    this.http.post<any>(url, dados).subscribe({
+      next: (response) => {
+        this.respostaApi = response; // Processa a resposta da API
+        console.log(this.respostaApi);
+      },
+      error: (err) => {
+        console.error('Erro ao enviar dados', err); // Lida com erros
+      }
+    });
   }
 }
