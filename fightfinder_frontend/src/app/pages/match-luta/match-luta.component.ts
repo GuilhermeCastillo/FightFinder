6458d5b12389395b5034fc26239986ed228c766b
Guilheme Castilho <guilherme.castilhot2c18@gmail.com>
2024-10-28 10:01:10 -0300

    renderizacao recomendacoes
    eu sou o cara

--- fightfinder_frontend/src/app/pages/match-luta/match-luta.component.ts
+++ fightfinder_frontend/src/app/pages/match-luta/match-luta.component.ts
@@ -4,9 +4,13 @@ import { FooterComponent } from '../../components/footer/footer.component';
 import { DropdownComponent } from '../../components/dropdown/dropdown.component';
 import { ButtonComponent } from '../../components/button/button.component';
 import { BotaoPequenoComponent } from '../../components/botao-pequeno/botao-pequeno.component';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 import { LoadingCircleComponent } from '../../components/loading-circle/loading-circle.component';
-import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { url } from 'inspector';
 import { TokenService } from '../../services/token/token.service';
@@ -14,11 +18,18 @@ import { TokenService } from '../../services/token/token.service';
 @Component({
   selector: 'app-match-luta',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, DropdownComponent, ButtonComponent, BotaoPequenoComponent, CommonModule, LoadingCircleComponent,
-    HttpClientModule
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    DropdownComponent,
+    ButtonComponent,
+    BotaoPequenoComponent,
+    CommonModule,
+    LoadingCircleComponent,
+    HttpClientModule,
   ],
   templateUrl: './match-luta.component.html',
-  styleUrl: './match-luta.component.css'
+  styleUrl: './match-luta.component.css',
 })
 export class MatchLutaComponent {
   modalidadeSelecionada: string = 'option';
@@ -28,43 +39,49 @@ export class MatchLutaComponent {
   valorPadraoDropdown: string = 'Modalidade';
   dados: any;
   respostaApi: any;
+  adversarioAtual: any;
+  indiceAtual: number = 0;
 
-  constructor(private http: HttpClient, private tokenService: TokenService) {  }
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
 
   onOptionSelected(option: string) {  
-    // limpar ou sobreescrever valores do adversário ao chamar a função 
-    this.modalidadeSelecionada = option;
-    if (option != this.valorPadraoDropdown) {
-      this.botaoDesabilitado = false; 
-      return;
-    }
-    this.botaoDesabilitado = true;
+      this.modalidadeSelecionada = option;
+      this.botaoDesabilitado = option === this.valorPadraoDropdown;
   }
 
   buscarAdversarios() { 
-    // if (this.modalidadeSelecionada == this.valorPadraoDropdown) { 
-    //   this.carregarAdversarios = false;
-    //   return;
-    // } 
-    this.carregarAdversarios = true;
- 
-    const url = 'http://127.0.0.1:8000/api/v1/recommend/';
-    let token = this.tokenService.getToken();
+      this.carregarAdversarios = true;
+  
+      const url = 'http://127.0.0.1:8000/api/v1/recommend/';
+      let token = this.tokenService.getToken();
 
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}` // ou 'Token ${token}' dependendo da sua API
-    });
+      const headers = new HttpHeaders({
+          'Authorization': `Bearer ${token}`
+      });
 
-    this.http.get<any>(url, { headers }).subscribe({
-      next: (response) => {
-        this.respostaApi = response;
-        console.log("RETORNO", this.respostaApi);
-      },
-      error: (err) => {
-        console.error('Erro ao enviar dados', err);
-      }
-    });
+      this.http.get<any>(url, { headers }).subscribe({
+          next: (response) => {
+              this.respostaApi = response;
+              this.indiceAtual = 0;
+              this.atualizarAdversarioAtual();
+          },
+          error: (err) => {
+              console.error('Erro ao carregar adversários', err);
+          }
+      });
   }
 
-} 
+  atualizarAdversarioAtual() {
+      // Verifica se há mais recomendações a serem exibidas
+      if (this.respostaApi.recommendations && this.indiceAtual < this.respostaApi.recommendations.length) {
+          this.adversarioAtual = this.respostaApi.recommendations[this.indiceAtual];
+      } else {
+          this.adversarioAtual = null; // Não há mais adversários para exibir
+      }
+  }
 
+  proximoAdversario() {
+      this.indiceAtual++;
+      this.atualizarAdversarioAtual();
+  }
+}
